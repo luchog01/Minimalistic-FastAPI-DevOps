888812629ab7588052467ff35a116204785c6cb7
Luciano Garbarino <lucigar01@gmail.com>
2024-08-06 01:34:16 -0300

    first staging version (#1)
    * Create ci-pipeline.yml
    
    * fix: devcontainer compose file dir
    
    * fix: CI Pipeline
    
    * feat: pre-commit hooks
    
    * fix: format
    
    * feat: root endpoint health check

--- app/main.py
+++ app/main.py
@@ -5,13 +5,21 @@ from . import database
 
 app = FastAPI()
 
+
+@app.get("/")
+async def healthcheck():
+    return {"status": "ok"}
+
+
 @app.post("/matches", response_model=Match)
 async def create_match(match: MatchCreate):
     return database.create_match(match)
 
+
 @app.get("/matches", response_model=list[Match])
 async def get_matches():
     return database.get_matches()
+
 
 @app.get("/matches/{match_id}", response_model=Match)
 async def get_match(match_id: str):
--- app/models.py
+++ app/models.py
@@ -1,11 +1,13 @@
 # app/models.py
 from pydantic import BaseModel
 
+
 class MatchCreate(BaseModel):
     name: str
     team1: str
     team2: str
     result: int
+
 
 class Match(MatchCreate):
     id: str
